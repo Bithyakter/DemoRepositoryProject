d3e04b37d1693252855845c45f1deb3e6f783203
Mst Bithy <mstbithy.cse@gmail.com>
2022-01-09 13:46:03 +0600

    LinkChange

--- DemoRepositoryProject/Startup.cs
+++ DemoRepositoryProject/Startup.cs
@@ -59,7 +59,7 @@ namespace DemoRepositoryProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Student}/{action=Index}/{id?}");
             });
         }
     }
